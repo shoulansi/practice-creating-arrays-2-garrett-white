142ea69038df7113129fce1451dc2b6127070fa1
shoulansi <garrettswhite@gmail.com>
2025-03-19 21:16:32 -0500

    final array practice 2

--- practice-creating-arrays-2.js
+++ practice-creating-arrays-2.js
@@ -1,1 +1,14 @@
+let helloArr = Array(7);
+let filledHelloArr = helloArr.fill('Hello');
+console.log(filledHelloArr);
 
+filledHelloArr.fill("hey", 3, 6);
+console.log(filledHelloArr);
+
+let loopArr = Array(5);
+
+for(let i = 0; i < loopArr.length; i++) {
+    loopArr[i] = i * 10;
+}
+
+console.log(loopArr);
